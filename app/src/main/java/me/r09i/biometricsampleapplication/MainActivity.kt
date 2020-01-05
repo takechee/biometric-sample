ec65935483278eddfc0c213e3190176e618ba4a2
Ryo Takeuchi <takechee14@gmail.com>
2020-01-05 13:21:28 +0900

    生体認証ロジック追加

--- app/src/main/java/me/r09i/biometricsampleapplication/MainActivity.kt
+++ app/src/main/java/me/r09i/biometricsampleapplication/MainActivity.kt
@@ -5,8 +5,11 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +18,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            when (BiometricManager.from(this).canAuthenticate()) {
+                BiometricManager.BIOMETRIC_SUCCESS -> {
+                    /* 生体認証が利用可能 */
+                    biometric()
+                }
+
+                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                    /* 生体情報が端末に登録されていない */
+                    showSnackbar("生体情報が端末に登録されていない")
+                }
+
+                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE,
+                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                    /* 生体認証ハードウェアが利用不可 */
+                    showSnackbar("生体認証ハードウェアが利用不可")
+                }
+
+                else -> showSnackbar("Unknown")
+            }
         }
     }
 
@@ -36,4 +56,43 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private fun biometric() {
+        val promptInfo = BiometricPrompt.PromptInfo
+            .Builder()
+            .setTitle("タイトル") // 必須項目
+            .setSubtitle("サブタイトル") // 任意
+            .setDescription("説明文") // 任意
+            .setNegativeButtonText("CANCEL") // 任意
+            .build()
+
+        val executor = Executors.newSingleThreadExecutor()
+        val biometricPrompt = BiometricPrompt(
+            this,
+            executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    // 成功した場合の処理
+                    showSnackbar("認証成功")
+                }
+
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    showSnackbar("エラーコード：$errorCode, $errString")
+                }
+
+                override fun onAuthenticationFailed() {
+                    showSnackbar("認証に失敗しました")
+                }
+            })
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showSnackbar(text: String, length: Int = Snackbar.LENGTH_LONG) {
+        Snackbar.make(fab, text, length).show()
+    }
+
 }
